5690fb0cc31f9339e4edd182d36e0109de3529e9
Maxim <mmassolit@gmail.com>
2020-11-24 09:46:31 +0200

    Bugs fixed

--- paymentService/src/main/java/com/videosharing/service/impl/UserService.java
+++ paymentService/src/main/java/com/videosharing/service/impl/UserService.java
@@ -6,9 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.videosharing.api.dto.UserPayload;
@@ -31,12 +28,7 @@ public class UserService implements IUserService {
     public List<User> findAll() {
         return (List<User>) repository.findAll();
     }
-    
-    @Override
-    public Page<User> findPaginated(int page, int size) {
-        return repository.findAll(PageRequest.of(page, size, Sort.by("dateCreated")));
-    }
-    
+
     @Override
     public User save(User userForSave) {
         return repository.save(userForSave);
